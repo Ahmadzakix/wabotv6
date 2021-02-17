20ec178322301638897d0c4caad97c558701e1ce
Ahmadzakix <78718098+Ahmadzakix@users.noreply.github.com>
2021-02-17 23:17:20 +0700

    Add files via upload

---
+++ database/menu/premium.js
@@ -0,0 +1,35 @@
+const vip = (pushname, prefix, botName, ownerName, getLevelingLevel, sender, _registered) => {
+	return `
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 0.0.5
+   ╿
+┯┷ *〈 ABOUT 〉*
+╽
+┠≽ *${prefix}info* (error)
+┃ *Desc* : Show Bot Details
+┠──────────────╼
+┠≽ *${prefix}blocklist*
+┃ *Desc* : Show Blocked User
+┠──────────────╼
+┠≽ *${prefix}chatlist* (error)
+┃ *Desc* : Show All Chat User
+┠──────────────╼
+┠≽ *${prefix}ping*
+┃ *Desc* : Show Connection Bot Speed
+┠──────────────╼
+┠≽ *${prefix}bugreport* <text>
+┃ *Desc* : Report Bug To Owner Bot
+╿
+┷┯ *〈 PREMIUM MENU〉*
+   ╽
+   ┠≽ *${prefix}Branily* <country>
+   ┃ *Desc* : Jawaban Tugas
+   ╿ *${ownerName}*,
+   ╰╼≽ *Developer © ${botName}`
+}
+exports.vip = vip
